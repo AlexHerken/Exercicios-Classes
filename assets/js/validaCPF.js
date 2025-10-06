6b6940968057f4c4369fefadf3eb356c688eccce
Alex <alexxherken2@gmail.com>
2025-10-06 14:28:38 -0300

    criando validações

--- assets/js/validaCPF.js
+++ assets/js/validaCPF.js
@@ -9,17 +9,26 @@ class ValidaCPF {
         );
     }
 
-    
+    verificaSequencia() {
+        return this.cpfLimpo.charAt(o).repeat(11) === this.cpfLimpo;
+    }
+
+    gerarNovoCpf() {
+        const cpfSemDigitos = this.cpfLimpo.slice(0, -2);
+        const digito1 = this.geraDigito(cpfSemDigitos);
+    }
+
 
     valida() {
 
-        if(!this.cpfLimpo) return false;
+        if(!this.cpfLimpo) return false; //verifica se está vazio
+        if(typeof cpfLimpo !== 'string') return false; // verificar se é diferente de uma string
+        if(this.cpfLimpo,length !== 11) return false; // verificar se o tamanho é diferente de 11
+        if(this.verificaSequencia()) return false; // verifica se é o mesmo caractere
+        if(!this.gerarNovoCpf()) return false;
 
-        if(typeof cpfLimpo !== 'string') return false;
 
-        if(this.cpfLimpo,length !== 11) return false;
-
-        return;
+        return 'FIM';
     }
 
 }
