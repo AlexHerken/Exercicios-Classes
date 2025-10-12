ce4a49f647c31177becad1111cd56651c6b737e9
Alex <alexxherken2@gmail.com>
2025-10-12 19:50:47 -0300

    Alterações no arquivo js

--- assets/js/validaCPF.js
+++ assets/js/validaCPF.js
@@ -4,33 +4,56 @@ class ValidaCPF {
             writable: false,
             enumerable: false,
             configurable: false,
-            value: cpfEnviado.replace(/\D+/g, '')
+            value: cpfEnviado.replace(/\D+/g, '')// utiliza uma expressão regular para limpar os dígitos
         }
         );
     }
 
     verificaSequencia() {
-        return this.cpfLimpo.charAt(o).repeat(11) === this.cpfLimpo;
+        return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo; // verifica se a string toda é igual
     }
 
     gerarNovoCpf() {
-        const cpfSemDigitos = this.cpfLimpo.slice(0, -2);
-        const digito1 = this.geraDigito(cpfSemDigitos);
+        const cpfSemDigitos = this.cpfLimpo.slice(0, -2); // retorna uma string sem os últimos 2 digitos
+        const digito1 = ValidaCPF.geraDigito(cpfSemDigitos); // chama a função que calcula o 1 digito
+        const digito2 = ValidaCPF.geraDigito(cpfSemDigitos + digito1); // chama a função para calcular o segundo digito
+        this.novoCpf = cpfSemDigitos + digito1 + digito2; //concatena o cpf sem digito com os 2 digitos
+    }
+
+    static geraDigito(cpfSemDigitos) { //passa o cpf sem digitos e depois com 1 digito para serem calculados
+        let total = 0;
+        let reverso = cpfSemDigitos.length + 1; // faz o calculo de 10 a 2 e depois 11 a 2
+
+        for(let stringNum of cpfSemDigitos) {
+            total += reverso * Number(stringNum);
+            reverso --;
+        }
+
+        const digito = 11 - (total % 11); // calcula 11 - o resto da divisao do total
+
+        return digito <= 9 ? String(digito) : '0'; //se o valor for menor que 10 o valor é o digito se for 10 ou 11 é 0
+
     }
 
 
     valida() {
 
         if(!this.cpfLimpo) return false; //verifica se está vazio
-        if(typeof cpfLimpo !== 'string') return false; // verificar se é diferente de uma string
-        if(this.cpfLimpo,length !== 11) return false; // verificar se o tamanho é diferente de 11
+        if(typeof this.cpfLimpo !== 'string') return false; // verificar se é diferente de uma string
+        if(this.cpfLimpo.length !== 11) return false; // verificar se o tamanho é diferente de 11
         if(this.verificaSequencia()) return false; // verifica se é o mesmo caractere
-        if(!this.gerarNovoCpf()) return false;
+        this.gerarNovoCpf();
+        
 
-
-        return 'FIM';
+        return this.novoCpf === this.cpfLimpo;
     }
 
 }
 
 const validar = new ValidaCPF('123.456.789-09');
+
+if(validar.valida()){
+    console.log('CPF VÁLIDO');
+} else {
+    console.log('CPF inválido');
+}
